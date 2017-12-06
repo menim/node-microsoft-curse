20ca7b3074674a6cb05205353ef8495552c92276
Anatoliy <mel198629@gmail.com>
2017-12-06 17:59:06 +0200

    end of the first module

--- csv-to-json-converter/convert-to-json.js
+++ csv-to-json-converter/convert-to-json.js
@@ -1,18 +1,41 @@
 const fs = require('fs');
 const path = require('path');
 
-let buff=''
+let buff = [];
 
-const file = fs.readFile(path.join(__dirname,'customer-data.csv'),'utf-8', (error, data) => {
-	let dataToArr = data.split('\n');
-    let propNameArr = dataToArr[0].split(',');
-    let propValueArr = dataToArr.splice(1);
-    propValueArr.forEach((item) => {
-    	item = item.split(',');
-       console.log(item)
-    });
+let CSVcontent = fs.readFileSync(path.join(__dirname,'customer-data.csv'),'utf-8');
 
-   // console.log(propValueArr, propNameArr);
-});
+function splitAny(data,separator){  
+  return data.split(separator);
+}
+
+function converCSVToJson(data){
+  let dataToArr = splitAny(data, '\n');
+  let propNameArr = splitAny(dataToArr[0],',');
+  let propValueArr = dataToArr.splice(1);
+  let temp = {};
+  let res  = [];
+  propValueArr = propValueArr.map((item) => {
+    return splitAny(item, ',');
+  });
+
+  propValueArr.forEach((item) => { 
+  for(let i = 0; i<=propNameArr.length-1; i++){
+    temp[propNameArr[i]] = item[i];
+  }
+  res.push(Object.assign({},temp));
+  });
+  return res;
+}
+
+let jsonData = JSON.stringify(converCSVToJson(CSVcontent),null,2);
+
+fs.writeFile(path.join(__dirname,'customer-data.json'),jsonData,(err, data) => {
+  if(err){
+  	throw err;
+  }
+})
+
+
 
 
