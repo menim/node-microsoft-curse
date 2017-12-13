bd31a8faa73c093dbcaf1b2e0d8844d80abf5c64
Anatoliy <mel198629@gmail.com>
2017-12-13 18:28:25 +0200

    add middleware use to express-hello-world and add npm middlware body-parser and morgan

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+app.use(bodyParser.json()); 
+app.use(morgan('dev')); 
+
+app.use((req,res,next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.use((req,res,next) => {
+ if(req.query.api_key){
+   next();
+ }else{
+   res.status(401).send({msg: 'not authorised'})
+ }
+});
 
 app.get('/',(req,res) => {
   res.send('Hello world /')
+});
+
+app.get('/accounts',(req,res) => {
+  res.send({msg: 'accounts'})
+});
+
+app.post('/transactions',(req,res) => {
+	console.log(req.body);
+  res.send({msg: 'transactions'})
 });
 
 app.listen(3000, () => {
